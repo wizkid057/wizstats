50807a603b3ceaaccbaa18991b04084824b10c61
Jason Hughes <wizkid057@gmail.com>
2013-07-13 04:45:06 +0000

    Filter out balances < 2 TBC

--- payoutqueue.php
+++ payoutqueue.php
@@ -28,8 +28,15 @@ print "<H2>Payout Queue</H2><BR>";
 	$total = 0; $alltotal = 0; $blocks = 1;
 	$oddeven = 1; $oe = " class=\"blockconfirmed\"";
 	$paynum = 1;
+	$lt10tbc = 0;
+	$lt10tbcc = 0;
 	foreach($payout as $key) {
 		$value = $balance[$key];
+		if ($value['balance'] < 1048576) { 
+			$lt10tbc += $value['balance'];
+			$lt10tbcc++;
+			$value['balance'] = 0; 
+		}
 		while($value['balance'] > 0) {
 			if ($total+$value['balance'] > 2500000000) {
 				$maxbal = 2500000000 - $total;
@@ -55,6 +62,9 @@ print "<H2>Payout Queue</H2><BR>";
 	print "Total: ".prettySatoshis($alltotal)."<BR>";
 	print "Block Count: $blocks<BR><BR>";
 	print $qt;
+	if ($lt10tbcc) {
+		print "<SMALL>Note: There are also $lt10tbcc address".($lt10tbcc!=1?'es':'')." with timeout balances less than 10 TBC but more than 2 TBC, totalling ".prettySatoshis($lt10tbc)." which are not included in the automated payout queue and will be paid manually.</SMALL><BR><BR>";
+	}
 
 
 print_stats_bottom();
