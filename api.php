08f953b2f5fa79ad47d241029e238d61f7d39698
Jason Hughes <wizkid057@gmail.com>
2013-08-22 03:53:30 +0000

    api.php: beginning of new API

---
+++ api.php
@@ -0,0 +1,121 @@
+<?php
+
+require_once 'includes.php';
+
+if (isset($_GET["format"])) {
+	$format = strtolower($_GET["format"]);
+	#if (($format != "json") && ($format != "text") && ($format != "html")) {
+	if (($format != "json") && ($format != "text")) {
+		$format = "json";
+		header("Content-type: application/json");
+		ws_api_error("Invalid format");
+	}
+} else {
+	$format = "json";
+}
+
+if ($format == "json") { header("Content-type: application/json"); }
+if ($format == "text") { header("Content-type: text/plain"); }
+#if ($format == "html") { header("Content-type: text/html"); }
+
+
+if (isset($_GET["cmd"])) {
+	$cmd = strtolower($_GET["cmd"]);
+} else {
+	ws_api_error("No command");
+}
+
+$data = array("cmd" => $cmd, "stime" => time());
+
+
+if ($cmd == "getacceptedcount") {
+	$link = pg_pconnect("dbname=$psqldb user=$psqluser password='$psqlpass' host=$psqlhost");
+	if ((isset($_GET["user_id"])) && (is_numeric($_GET["user_id"])) ) {
+		$user_id =  pg_escape_string($_GET["user_id"]);
+	} else {
+		ws_api_error("$cmd: No valid user id specified");
+	}
+	if (isset($_GET["startdate"])) {
+		$startdate = pg_escape_string($link, $_GET["startdate"]);
+		if (($_GET["startdate"] != $startdate) || (strtotime($startdate) < 1368483761)) { ws_api_error("$cmd: Invalid start date"); }
+	} else {
+		$startdate = "NOW()-'3 hours'::interval";
+	}
+	if (isset($_GET["enddate"])) {
+		$enddate = pg_escape_string($link, $_GET["enddate"]);
+		if (($_GET["enddate"] != $enddate) || (strtotime($enddate) < 1368483761)) { ws_api_error("$cmd: Invalid end date"); }
+	}
+
+	$sql = "select sum(accepted_shares) as count,sum(rejected_shares) as rcount,max(time) as ltime,min(time) as ftime from $psqlschema.stats_shareagg where server=$serverid and user_id=$user_id and time >= '$startdate' ".(isset($enddate)?" and time <= '$enddate'":"").";";
+	$result = pg_exec($link, $sql);
+	$numrows = pg_numrows($result);
+	if (!$numrows) {
+		ws_api_error("$cmd: No result");
+	}
+
+	$row = pg_fetch_array($result, 0);
+	$count = $row["count"];
+	$rcount = $row["rcount"];
+	$ftime = $row["ftime"];
+	$ltime = $row["ltime"];
+	$data["output"] = array("user_id" => $user_id, "startdate" => $startdate, "enddate" => $enddate, "accepted" => $count, "rejected" => $rcount, "first_row_date" => $ftime, "last_row_date" => $ltime);
+	echo ws_api_encode($data);
+	exit;
+}
+
+
+if ($cmd == "getuseroptions") {
+	# dump full My Eligius options
+	$link = pg_pconnect("dbname=$psqldb user=$psqluser password='$psqlpass' host=$psqlhost");
+	$sql = "select (r).*,username from (select (select t from $psqlschema.stats_mystats t where t.user_id=u.id order by time desc limit 1) as r from users u offset 0) s left join users on (r).user_id=users.id where (r).user_id is not null and (r).server=$serverid order by id desc;";
+	$result = pg_exec($link, $sql);
+	$numrows = pg_numrows($result);
+	$output = array("count" => $numrows);
+	if ($numrows) {
+		for($i=0;$i<$numrows;$i++) {
+			$row = pg_fetch_array($result, $i);
+			# strip worker names...
+			$username = substr($row["username"],0,strpos($row["username"],"_")?strpos($row["username"],"_"):128);
+			$options = $row["signed_options"];
+			$sig = $row["signature"];
+			$stime = $row["time"];
+			$sid = $row["id"];
+			$suid = $row["user_id"];
+			$msghead = "My ".$poolname." - ";
+			$msgvars = substr($options,strlen($msghead)+26,10000);
+			$msgvars = str_replace(" ","&",$msgvars);
+			parse_str($msgvars, $msgvars_array);
+			$line = array("username" => $username, "user_id" => $suid, "dbid" => $sid, "dbtime" => $stime, "options" => $options, "sig" => $sig, "clean_options" => $msgvars_array);
+			array_push($output,$line);
+		}
+	}
+	$data["output"] = $output;
+	echo ws_api_encode($data);
+
+	exit;
+
+}
+
+
+
+ws_api_error("Command not found");
+
+
+function ws_api_encode($data) {
+	if ($GLOBALS["format"] == "json") {
+		return json_encode($data);
+	}
+	if ($GLOBALS["format"] == "text") {
+		print_r($data);
+	}
+
+}
+
+
+function ws_api_error($msg) {
+
+	if (strlen($msg) == 0) { $msg = "Unknown"; }
+	$data = array("error" => $msg, "stime" => time());
+	echo ws_api_encode($data);
+	exit;
+}
