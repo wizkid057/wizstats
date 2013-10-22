471e71452f61b7b8e4dd314e06d0915fe584c875
Jason Hughes <wizkid057@gmail.com>
2013-10-22 02:19:45 +0000

    Optimize update shares.  Also make sure it doesnt try to aggregate shares we dont have yet.

--- updateshares.php
+++ updateshares.php
@@ -22,17 +22,23 @@ if( isLocked() ) die( "Already running.\n" );
 
 $link = pg_Connect("dbname=$psqldb user=$psqluser password='$psqlpass' host=$psqlhost", PGSQL_CONNECT_FORCE_NEW );
 
+
+$sql = "select to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) as lst from public.shares where server=$serverid order by id desc limit 1";
+$result = pg_exec($link, $sql);
+$row = pg_fetch_array($result, 0);
+$latestsharetime = $row["lst"];
+
+$sql = "select to_timestamp(((date_part('epoch', (select time from wizkid057.stats_shareagg order by time desc limit 1))::integer / 675::integer) * 675::integer)+675::integer) as fst";
+$result = pg_exec($link, $sql);
+$row = pg_fetch_array($result, 0);
+$firstsharetime = $row["fst"];
+
 # All the work for this is done by postgresql, which is nice, under this query
-$sql = "INSERT INTO $psqlschema.stats_shareagg 
-(server, time, user_id, accepted_shares, rejected_shares, blocks_found, hashrate) 
-select server, 
-to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) AS ttime, 
-user_id, 
-0+SUM(((our_result::integer) * pow(2,(targetmask-32)))) as acceptedshares, 
-COUNT(*)-SUM(our_result::integer) as rejectedshares, 
-SUM(upstream_result::integer) as blocksfound, 
-((SUM(((our_result::integer) * pow(2,(targetmask-32)))) * POW(2, 32)) / 675) AS hashrate 
-from public.shares where time > to_timestamp(((date_part('epoch', (select time from $psqlschema.stats_shareagg order by time desc limit 1))::integer / 675::integer) * 675::integer)+675::integer) and to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) < to_timestamp((date_part('epoch', NOW())::integer / 675::integer) * 675::integer) and server=$serverid group by ttime, server, user_id;";
+$sql = "INSERT INTO $psqlschema.stats_shareagg (server, time, user_id, accepted_shares, rejected_shares, blocks_found, hashrate)
+select server, to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) AS ttime, user_id,
+0+SUM(((our_result::integer) * pow(2,(targetmask-32)))) as acceptedshares, COUNT(*)-SUM(our_result::integer) as rejectedshares, SUM(upstream_result::integer) as blocksfound,
+((SUM(((our_result::integer) * pow(2,(targetmask-32)))) * 4294967296) / 675) AS hashrate
+from public.shares where time > '$firstsharetime' and to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) < '$latestsharetime' and server=$serverid group by ttime, server, user_id;";
 $result = pg_exec($link, $sql);
 
 unlink( LOCK_FILE );
