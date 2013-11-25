4d61e4c4eda0a6d1b8380a9b97cd251893e30fcf
Jason Hughes <wizkid057@gmail.com>
2013-11-25 00:39:52 +0000

    sql optimiziation

--- topcontributors.php
+++ topcontributors.php
@@ -41,12 +41,16 @@ if ($cacheddata != "") {
 	if (!isset($link2)) { $link2 = pg_pconnect("dbname=$psqldb user=$psqluser password='$psqlpass' host=$psqlhost"); }
 
 	# get total pool hashrate
-	$sql = "select (sum(accepted_shares)*pow(2,32))/10800 as avghash from $psqlschema.stats_shareagg where server=$serverid and time > to_timestamp((date_part('epoch', (select time from $psqlschema.stats_shareagg where server=$serverid group by server,time order by time desc limit 1))::integer / 675::integer)::integer * 675::integer)-'3 hours'::interval";
+	$sql = "select to_timestamp((date_part('epoch', (time))::integer / 675::integer)::integer * 675::integer)-'3 hours'::interval as stime from $psqlschema.stats_shareagg where server=$serverid group by server,time order by time desc limit 1";
+	$result = pg_exec($link, $sql); $row = pg_fetch_array($result, 0);
+	$stime = $row["stime"];
+
+	$sql = "select (sum(accepted_shares)*pow(2,32))/10800 as avghash from $psqlschema.stats_shareagg where server=$serverid and time > '$stime'::timestamp without time zone";
 	$result = pg_exec($link, $sql); $row = pg_fetch_array($result, 0);
 	$poolhashrate3hr = $row["avghash"];
 
 
-	$sql = "select (sum(accepted_shares)*pow(2,32))/10800 as avghash, sum(accepted_shares) as sharecount, keyhash, min(users.id) as user_id from $psqlschema.stats_shareagg left join users on user_id=users.id where server=$serverid and time > to_timestamp((date_part('epoch', (select time from $psqlschema.stats_shareagg where server=$serverid group by server,time order by time desc limit 1)-'3 hours'::interval)::integer / 675) * 675) and accepted_shares > 0 group by keyhash order by avghash desc $minilimit;";
+	$sql = "select (sum(accepted_shares)*pow(2,32))/10800 as avghash, sum(accepted_shares) as sharecount, keyhash, min(users.id) as user_id from $psqlschema.stats_shareagg left join users on user_id=users.id where server=$serverid and time > '$stime'::timestamp without time zone and accepted_shares > 0 group by keyhash order by avghash desc $minilimit;";
 	$result = pg_exec($link, $sql);
 	$numrows = pg_numrows($result);
 
