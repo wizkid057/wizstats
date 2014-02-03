2baae8196e76e0d06a147e035c834cc471a0090e
Jason Hughes <wizkid057@gmail.com>
2014-02-03 01:30:42 +0000

    Add getblocks api command, add caching, add limitted csv output ability

--- api.php
+++ api.php
@@ -20,6 +20,14 @@
 require_once 'includes.php';
 require_once 'lib.userstat.php';
 
+$allow_csv = array('getblocks' => 1);
+
+if (isset($_GET["cmd"])) {
+	$cmd = strtolower($_GET["cmd"]);
+} else {
+	$cmd = "";
+}
+
 if (isset($_GET["format"])) {
 	$format = strtolower($_GET["format"]);
 	if ($format == "jsonp") {
@@ -32,10 +40,19 @@ if (isset($_GET["format"])) {
 	} else {
 		$callback = 0;
 	}
-	if (($format != "json") && ($format != "text")) {
-		$format = "json";
-		header("Content-type: application/json");
-		ws_api_error("Invalid format");
+
+	if ($format == "csv") {
+		if (!array_key_exists($cmd,$allow_csv)) {
+			$format = "json";
+			header("Content-type: application/json");
+			ws_api_error("Command does not support CSV output");
+		}
+	} else {
+		if (($format != "json") && ($format != "text")) {
+			$format = "json";
+			header("Content-type: application/json");
+			ws_api_error("Invalid format");
+		}
 	}
 } else {
 	$format = "json";
@@ -49,15 +66,29 @@ if ($format == "json") {
 	}
 }
 if ($format == "text") { header("Content-type: text/plain"); }
+if ($format == "csv") { 
+	if (isset($_GET["csvastext"])) {
+		header("Content-type: text/plain"); 
+	} else {
+		header("Content-type: text/csv"); 
+		header('Content-Disposition: attachment;filename='.$cmd.'.csv');
+	}
+}
 
-if (isset($_GET["cmd"])) {
-	$cmd = strtolower($_GET["cmd"]);
-} else {
+if ($cmd == "") {
 	ws_api_error("No command");
 }
 
 $data = array("cmd" => $cmd, "stime" => time());
 
+
+# TODO - replace with more limitted caching...
+$qshash = hash("sha256", $_SERVER['QUERY_STRING']);
+if ($data = apc_fetch("api.php - $cmd - $qshash")) {
+	# this call is cached
+	echo ws_api_encode($data);
+	exit;
+}
 
 if ($cmd == "getacceptedcount") {
 	$link = pg_pconnect("dbname=$psqldb user=$psqluser password='$psqlpass' host=$psqlhost");
@@ -222,17 +253,158 @@ if ($cmd == "getblocks") {
 
 	if ((isset($_GET["limit"])) && (is_numeric($_GET["limit"])) ) {
 		$limit =  pg_escape_string($_GET["limit"]);
+		if ($limit < 0) { $limit = 1; }
 	} else {
 		$limit = 32;
 	}
 
 	if ((isset($_GET["offset"])) && (is_numeric($_GET["offset"])) ) {
 		$offset =  pg_escape_string($_GET["offset"]);
+		if ($offset < 0) { $offset = 0; }
 	} else {
 		$offset = 0;
 	}
 
+	if ((isset($_GET["minconfs"])) && (is_numeric($_GET["minconfs"])) ) {
+		$minconfs = pg_escape_string($_GET["minconfs"]);
+		if ($minconfs < 0) { $minconfs = 0; }
+	} else {
+		$minconfs = 1;
+	}
 
+	if ((isset($_GET["minheight"])) && (is_numeric($_GET["minheight"])) ) {
+		$minheight = pg_escape_string($_GET["minheight"]);
+		if ($minheight < 0) { $minheight = 0; }
+	} else {
+		$minheight = 0;
+	}
+
+	$sortorder = "desc";
+	if ((isset($_GET["sortorder"])) && (is_numeric($_GET["sortorder"])) ) {
+		if ($_GET["sortorder"] == 1) {
+			$sortorder = "asc";
+		}
+	}
+	$showpretty = 1;
+	if ((isset($_GET["showpretty"])) && (is_numeric($_GET["showpretty"])) ) {
+		if ($_GET["showpretty"] == 0) {
+			$showpretty = 0;
+		}
+	}
+
+	if (isset($_GET["sortby"])) {
+		$askby = strtolower($_GET["sortby"]);
+
+		# doing this to fully limit and allow advanced sorts
+		switch($askby) {
+			case "accepted_shares":
+				$sortby = "acceptedshares";
+				$sortby_clean = "accepted_shares";
+				break;
+			case "network_difficulty":
+				$sortby = "network_difficulty $sortorder, time";
+				$sortby_clean = "network_difficulty";
+				break;
+			case "percent_of_network":
+				$sortby = "((acceptedshares/(date_part('epoch', time)::integer-date_part('epoch', roundstart)::integer))/network_difficulty)";
+				$sortby_clean = "percent_of_network";
+				break;
+			case "height":
+				$sortby = "height";
+				$sortby_clean = "height";
+				break;
+			case "duration":
+				$sortby = "(time-roundstart)";
+				$sortby_clean = "duration";
+				break;
+			case "hashrate":
+				$sortby = "(acceptedshares/(date_part('epoch', time)::integer-date_part('epoch', roundstart)::integer))";
+				$sortby_clean = "hashrate";
+				break;
+			case "luck":
+				$sortby = "(network_difficulty/acceptedshares)";
+				$sortby_clean = "luck";
+				break;
+			case "time":
+			case "age":
+			case "roundstart":
+			case "roundend":
+			default:
+				$sortby = "time";
+				$sortby_clean = "time";
+				break;
+		}
+	} else {
+		$sortby = "time";
+		$sortby_clean = "time";
+	}
+
+	$link = pg_pconnect("dbname=$psqldb user=$psqluser password='$psqlpass' host=$psqlhost");
+	$sql = "select stats_blocks.id as blockid,network_difficulty,confirmations,height,orig_id,time,user_id,keyhash,solution,blockhash,roundstart,acceptedshares,date_part('epoch', NOW())::integer-date_part('epoch', time)::integer as age,date_part('epoch', time)::integer-date_part('epoch', roundstart)::integer as duration, date_part('epoch', time) as time_unix, date_part('epoch', roundstart) as start_unix from $psqlschema.stats_blocks left join users on user_id=users.id where confirmations >= $minconfs and height >= $minheight and server=$serverid order by $sortby $sortorder limit $limit offset $offset";
+	$result = pg_exec($link, $sql);
+	$numrows = pg_numrows($result);
+
+	$output = array();
+	$output["count"] = $numrows;
+	$output["options"] = array();
+	$output["options"]["limit"] = $limit;
+	$output["options"]["offset"] = $offset;
+	$output["options"]["sortby"] = $sortby_clean;
+	$output["options"]["minconfs"] = $minconfs;
+	$output["options"]["minheight"] = $minheight;
+	$output["options"]["showpretty"] = $showpretty;
+	$output["options"]["sortorder"] = $sortorder=="desc"?"0":"1";
+	if ($showpretty) { $output["options"]["sortorder_pretty"] = $sortorder=="desc"?"highest_first":"lowest_first"; }
+
+	$output["rows"] = array();
+
+	for($ri = 0; $ri < $numrows; $ri++) {
+		$row = pg_fetch_array($result, $ri);
+		$output["rows"][$ri] = array();
+
+		if (isset($row["acceptedshares"])) { $luck = 100 * ($row["network_difficulty"] / $row["acceptedshares"]); } else { $luck = 0; }
+		if ($row["confirmations"] >= 120) { $row["confirmations"] = 120; }
+                list($seconds, $minutes, $hours) = extractTime($row["duration"]);
+                $seconds = sprintf("%02d", $seconds);
+                $minutes = sprintf("%02d", $minutes);
+                $hours = sprintf("%02d", $hours);
+		$hashrate = ($row["acceptedshares"] * 4294967296) / $row["duration"];
+
+		if (isset($row['keyhash'])) {
+			$fulladdress =  \Bitcoin::hash160ToAddress(bits2hex($row['keyhash']));
+		} else {
+			$fulladdress = "UNKNOWN_USER";
+		}
+
+		$output["rows"][$ri]["luck"] = $luck;
+		$output["rows"][$ri]["confirmations"] = $row["confirmations"];
+		$output["rows"][$ri]["blockhash"] = $row["blockhash"];
+		$output["rows"][$ri]["height"] = $row["height"];
+		$output["rows"][$ri]["duration"] = $row["duration"];
+		$output["rows"][$ri]["age"] = $row["age"];
+		$output["rows"][$ri]["roundstart"] = $row["start_unix"];
+		$output["rows"][$ri]["roundend"] = $row["time_unix"];
+		$output["rows"][$ri]["hashrate"] = sprintf("%u",$hashrate);
+		$output["rows"][$ri]["percent_of_network"] = 100*($hashrate / ($row["network_difficulty"]*7158278.82667));
+		$output["rows"][$ri]["accepted_shares"] = sprintf("%u",$row["acceptedshares"]);
+		$output["rows"][$ri]["network_difficulty"] = sprintf("%.4f",$row["network_difficulty"]);
+		$output["rows"][$ri]["block_finder"] = $fulladdress;
+		$output["rows"][$ri]["block_finder_user_id"] = $row["user_id"];
+
+		if ($showpretty) {
+			$output["rows"][$ri]["luck_pretty"] = number_format(round($luck,1),1)."%";
+			$output["rows"][$ri]["roundstart_pretty"] = substr($row["roundstart"],0,19);
+			$output["rows"][$ri]["roundend_pretty"] = substr($row["time"],0,19);
+			$output["rows"][$ri]["percent_of_network_pretty"] = number_format(round($output["rows"][$ri]["percent_of_network"],1),1)."%";
+			$output["rows"][$ri]["hashrate_pretty"] = prettyHashrate($hashrate);
+			$output["rows"][$ri]["age_pretty"] = prettyDuration($row["age"],false,1);
+			$output["rows"][$ri]["duration_pretty"] = "$hours:$minutes:$seconds";
+		}
+
+	}
+	$data["output"] = $output;
+	echo ws_api_encode($data);
+	exit;
 
 }
 
@@ -242,6 +414,10 @@ ws_api_error("Command not found");
 
 
 function ws_api_encode($data) {
+
+	# add to or replace in cache - keep time limitted for now...
+	apc_store("api.php - ".$GLOBALS["cmd"]." - ".$GLOBALS["qshash"], $data, 10);
+
 	if ($GLOBALS["format"] == "json") {
 		if ($GLOBALS["callback"]) {
 			$cb = preg_replace("/[^A-Za-z0-9_]/", '', $_GET['callback']); # why do people have to be so ruthless?!
@@ -252,6 +428,18 @@ function ws_api_encode($data) {
 	}
 	if ($GLOBALS["format"] == "text") {
 		print_r($data);
+	}
+
+	if ($GLOBALS["format"] == "csv") {
+		# compile header from the $data["output"]["rows"]["0"] field...
+		if (!isset($data["output"]["rows"]["0"])) {
+			return "Error no rows";
+		}
+		$fp = fopen('php://output', 'w');
+		fputcsv($fp, array_keys($data["output"]["rows"]["0"]));
+		foreach($data["output"]["rows"] as $row) {
+			fputcsv($fp, $row);
+		}
 	}
 
 }
